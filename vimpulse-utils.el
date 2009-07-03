688c1cee109e11670f1725a623479096e9bec1f1
Alessandro Piras <laynor@gmail.com>
2009-07-03 12:51:29 +0000

    Added a commodity macro to easily convert emacs region commands (like `comment-region') to commands that operate on vimpulse's visual selection (linewise and characterwise)
    M    trunk/vimpulse-utils.el

--- vimpulse-utils.el
+++ vimpulse-utils.el
@@ -30,7 +30,27 @@
 ;(viper-modify-major-mode 'help-mode 'emacs-state vimpulse-movement-map)
 
 
+(defmacro vimpulse-region-command (function)
+  "Commodity macro to convert emacs region commands to 
+vimpulse visual selection commands. See the comments on the source
+for an example on how to use it."
+  `(lambda ()
+     (interactive)
+     (,function (vimpulse-get-vs-start) (vimpulse-get-vs-end))
+     (vimpulse-visual-mode nil)))
 
+;;; The macro vimpulse-region-commands works with any emacs command that
+;;; operates with a region and takes as arguments the beginning and end of
+;;; the region. For example, the comment-region and uncomment-region commands:
+;;;        (comment-region beg end &optional arg)
+;;;       (uncomment-region beg end &optional arg)
+;;; You can define new bindings for comment region and uncomment region as 
+;;; easily as 
+;;
+;; (define-key viper-vi-global-user-map "\\\]" 
+;;   (vimpulse-region-command comment-region))
+;; (define-key viper-vi-global-user-map "\\," 
+;;   (vimpulse-region-command uncomment-region))
 
 
 (provide 'vimpulse-utils)
