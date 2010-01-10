aebb539123d09ad96d46ef54ecb16dfd0c67cc39
Vegard Øye <vegard_oye@hotmail.com>
2010-01-10 14:16:33 +0000

    Adding Windows batch file for "compiling" vimpulse-big.el

---
+++ compile.bat
@@ -0,0 +1,19 @@
+@echo off
+type Header ^
+     INSTALL ^
+     README ^
+     BUGS ^
+     TODO ^
+     Changelog ^
+     Acknowledgements ^
+     LICENSE ^
+     vimpulse-dependencies.el ^
+     vimpulse-viper-function-redefinitions.el ^
+     vimpulse-utils.el ^
+     vimpulse-misc-keybindings.el ^
+     vimpulse-ex.el ^
+     vimpulse-paren-matching.el ^
+     vimpulse-text-object-system.el ^
+     vimpulse-visual-mode.el ^
+     vimpulse.el ^
+   > vimpulse-big.el
