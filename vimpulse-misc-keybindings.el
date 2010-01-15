7c93b772e5702628db51684b05d3b17d31e91e55
epsilon47 <epsilon47@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-01-15 09:29:16 +0000

    Adding utility code for adding vi bindings to non-vi modes. Suggested bindings for Info mode are commented out.

--- vimpulse-misc-keybindings.el
+++ vimpulse-misc-keybindings.el
@@ -1,4 +1,101 @@
 ;;;;
+;;;; Utility code
+;;;;
+(defvar vimpulse-movement-cmds
+  '(viper-append viper-backward-Word viper-backward-char
+    viper-backward-paragraph viper-backward-sentence
+    viper-backward-word viper-beginning-of-line
+    viper-digit-argument viper-end-of-Word viper-end-of-word
+    viper-exec-mapped-kbd-macro viper-find-char-backward
+    viper-find-char-forward viper-forward-Word
+    viper-forward-char viper-forward-paragraph
+    viper-forward-sentence viper-forward-word
+    viper-goto-char-backward viper-goto-char-forward
+    viper-goto-eol viper-goto-line viper-insert
+    viper-line-to-bottom viper-line-to-middle
+    viper-line-to-top viper-next-line viper-previous-line
+    vimpulse-goto-first-line)
+  "List of Viper/Vimpulse movement commands.")
+
+(defvar vimpulse-core-movement-cmds
+  '(viper-backward-char
+    viper-next-line
+    viper-previous-line
+    viper-forward-char)
+  "List of Viper \"core\" movement commands.
+These should be present in every mode, to avoid confusion.")
+
+(defun vimpulse-augment-keymap
+  (map augment-alist &optional replace)
+  "Augment MAP with bindings from AUGMENT-ALIST.
+If REPLACE is non-nil, bindings in MAP may be overwritten.
+AUGMENT-ALIST has the format ((KEY . DEF) ...),
+where KEY and DEF are passed to `define-key'."
+  (let (binding key def)
+    (dolist (binding augment-alist)
+      (setq key (car binding)
+            def (cdr binding))
+      (when (or replace
+                (not (lookup-key map key))
+                (not (numberp (lookup-key map key))))
+        (define-key map key def)))))
+
+(defun vimpulse-add-vi-bindings (map cmds &optional replace)
+  "Add vi bindings for CMDS to MAP."
+  (let (cmd vimap pmap keys key)
+    (setq pmap (make-sparse-keymap))
+    (dolist (cmd cmds map)
+      (dolist (vimap (list viper-vi-intercept-map
+                           viper-vi-local-user-map
+                           viper-vi-global-user-map
+                           viper-vi-kbd-map
+                           viper-vi-diehard-map
+                           viper-vi-basic-map))
+        (setq keys (where-is-internal cmd vimap))
+        (dolist (key keys)
+          (unless (lookup-key pmap key)
+            (vimpulse-augment-keymap map
+                                     `((,key . ,cmd))
+                                     replace)
+            ;; To prioritize between maps in `vimap',
+            ;; we keep track of bindings by augmenting `pmap'.
+            (vimpulse-augment-keymap pmap
+                                     `((,key . ,cmd)))))))))
+
+(defun vimpulse-add-movement-cmds (map &optional replace)
+  "Add Viper/Vimpulse movement commands to MAP.
+The commands are taken from `vimpulse-movement-cmds' and looked
+up in vi keymaps. If REPLACE is non-nil, may overwrite bindings
+in MAP."
+  (vimpulse-add-vi-bindings map vimpulse-movement-cmds replace))
+
+;; The default for this function is to replace rather than augment,
+;; as core navigation should be present everywhere.
+(defun vimpulse-add-core-movement-cmds (map &optional augment)
+  "Add \"core\" movement commands to MAP, forcefully.
+The commands are taken from `vimpulse-core-movement-cmds'.
+If AUGMENT is non-nil, don't overwrite bindings in MAP."
+  (vimpulse-add-vi-bindings map
+                            vimpulse-core-movement-cmds
+                            (not augment)))
+
+(defun vimpulse-inhibit-movement-cmds (map &optional replace)
+  "Remap Viper movement commands to `viper-nil' in MAP.
+The commands are taken from `vimpulse-movement-cmds'.
+If REPLACE is non-nil, may overwrite bindings in MAP."
+  (let (cmd)
+    (dolist (cmd vimpulse-movement-cmds)
+      (eval `(vimpulse-augment-keymap
+              map '(([remap ,cmd] . viper-nil))
+              replace)))))
+
+;; Info mode
+;; (eval-after-load 'info
+;;   '(progn
+;;      (vimpulse-add-core-movement-cmds Info-mode-map)
+;;      (vimpulse-add-movement-cmds Info-mode-map)))
+
+;;;;
 ;;;; Almost all of this code is taken from extended-viper
 ;;;; coded by Brad Beveridge (bradbev@gmail.com)
 ;;;; - I changed the prefix of the custom functions to vimpulse
