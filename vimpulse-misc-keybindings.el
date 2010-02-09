f4200d83951792dd8b3a0366fbfc2a77a1890849
epsilon47 <epsilon47@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-02-09 14:42:36 +0000

    Adding N% functionality (jump to percentage in file). vimpulse-search-for-symbol-at-point now uses \_< and \_> only. All key bindings belong in viper-vi-basic-map; viper-vi-global-user-map is for users!

--- vimpulse-misc-keybindings.el
+++ vimpulse-misc-keybindings.el
@@ -1,7 +1,7 @@
 ;;;;
 ;;;; Utility code
 ;;;;
-(defvar vimpulse-movement-cmds
+(defvar vimpulse-viper-movement-cmds
   '(viper-append viper-backward-Word viper-backward-char
     viper-backward-paragraph viper-backward-sentence
     viper-backward-word viper-beginning-of-line
@@ -64,10 +64,10 @@ where KEY and DEF are passed to `define-key'."
 
 (defun vimpulse-add-movement-cmds (map &optional replace)
   "Add Viper/Vimpulse movement commands to MAP.
-The commands are taken from `vimpulse-movement-cmds' and looked
+The commands are taken from `vimpulse-viper-movement-cmds' and looked
 up in vi keymaps. If REPLACE is non-nil, may overwrite bindings
 in MAP."
-  (vimpulse-add-vi-bindings map vimpulse-movement-cmds replace))
+  (vimpulse-add-vi-bindings map vimpulse-viper-movement-cmds replace))
 
 ;; The default for this function is to replace rather than augment,
 ;; as core navigation should be present everywhere.
@@ -81,10 +81,10 @@ If AUGMENT is non-nil, don't overwrite bindings in MAP."
 
 (defun vimpulse-inhibit-movement-cmds (map &optional replace)
   "Remap Viper movement commands to `viper-nil' in MAP.
-The commands are taken from `vimpulse-movement-cmds'.
+The commands are taken from `vimpulse-viper-movement-cmds'.
 If REPLACE is non-nil, may overwrite bindings in MAP."
   (let (cmd)
-    (dolist (cmd vimpulse-movement-cmds)
+    (dolist (cmd vimpulse-viper-movement-cmds)
       (eval `(vimpulse-augment-keymap
               map '(([remap ,cmd] . viper-nil))
               replace)))))
@@ -94,6 +94,12 @@ If REPLACE is non-nil, may overwrite bindings in MAP."
 ;;   '(progn
 ;;      (vimpulse-add-core-movement-cmds Info-mode-map)
 ;;      (vimpulse-add-movement-cmds Info-mode-map)))
+
+(eval-after-load 'eldoc
+  (let (cmd)
+    (dolist (cmd (append vimpulse-viper-movement-cmds
+                        vimpulse-core-movement-cmds))
+      (eldoc-add-command cmd))))
 
 ;;;;
 ;;;; Almost all of this code is taken from extended-viper
@@ -110,14 +116,14 @@ If REPLACE is non-nil, may overwrite bindings in MAP."
   (add-hook 'hs-minor-mode-hook
             (lambda ()
               (call-interactively 'hs-hide-all)
-              (define-key viper-vi-global-user-map "za"
+              (define-key viper-vi-basic-map "za"
                 (lambda () (hs-toggle-hiding) (hs-hide-level h)))
-              (define-key viper-vi-global-user-map "zA"   'hs-toggle-hiding)
-              (define-key viper-vi-global-user-map "zM"   'hs-hide-all)
-              (define-key viper-vi-global-user-map "zR"   'hs-show-all)
-              (define-key viper-vi-global-user-map "zO" 'vimpulse-hs-Open)
-              (define-key viper-vi-global-user-map "zo"   'hs-show-block)
-              (define-key viper-vi-global-user-map "zc"   'hs-hide-block))))
+              (define-key viper-vi-basic-map "zA" 'hs-toggle-hiding)
+              (define-key viper-vi-basic-map "zM" 'hs-hide-all)
+              (define-key viper-vi-basic-map "zR" 'hs-show-all)
+              (define-key viper-vi-basic-map "zO" 'vimpulse-hs-Open)
+              (define-key viper-vi-basic-map "zo" 'hs-show-block)
+              (define-key viper-vi-basic-map "zc" 'hs-hide-block))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;; VISUAL MODE BINDINGS ;;;;;;;;;;;;;;;;
@@ -125,51 +131,51 @@ If REPLACE is non-nil, may overwrite bindings in MAP."
 (define-key viper-vi-basic-map "v" 'vimpulse-visual-mode)
 (define-key viper-vi-basic-map "V" 'vimpulse-visual-mode-linewise)
 
-(define-key viper-vi-global-user-map "K"    'woman)
-(define-key viper-vi-global-user-map "gd"   'vimpulse-goto-definition)
-(define-key viper-vi-global-user-map "gf"   'find-file-at-point)
-(define-key viper-vi-global-user-map "gg"   'vimpulse-goto-first-line)
-(define-key viper-vi-global-user-map "zb"   'viper-line-to-bottom)
-(define-key viper-vi-global-user-map "zh"   'scroll-right)
-(define-key viper-vi-global-user-map "zl"   'scroll-left)
-(define-key viper-vi-global-user-map "zt"   'viper-line-to-top)
-(define-key viper-vi-global-user-map "zz"   'viper-line-to-middle)
-(define-key viper-vi-global-user-map "*"    'vimpulse-search-forward-for-symbol-at-point)
-(define-key viper-vi-global-user-map "#"    'vimpulse-search-backward-for-symbol-at-point)
-(define-key viper-vi-global-user-map " "    nil)
-(define-key viper-vi-global-user-map "\C-]" 'vimpulse-jump-to-tag-at-point)
-(define-key viper-vi-global-user-map "\C-t" 'pop-tag-mark)
+(define-key viper-vi-basic-map "K" 'woman)
+(define-key viper-vi-basic-map "g" nil) ; delete `viper-nil' binding
+(define-key viper-vi-basic-map "gd" 'vimpulse-goto-definition)
+(define-key viper-vi-basic-map "gf" 'find-file-at-point)
+(define-key viper-vi-basic-map "gg" 'vimpulse-goto-first-line)
+(define-key viper-vi-basic-map "zb" 'viper-line-to-bottom)
+(define-key viper-vi-basic-map "zh" 'scroll-right)
+(define-key viper-vi-basic-map "zl" 'scroll-left)
+(define-key viper-vi-basic-map "zt" 'viper-line-to-top)
+(define-key viper-vi-basic-map "zz" 'viper-line-to-middle)
+(define-key viper-vi-basic-map "*" 'vimpulse-search-forward-for-symbol-at-point)
+(define-key viper-vi-basic-map "#" 'vimpulse-search-backward-for-symbol-at-point)
+(define-key viper-vi-basic-map "\C-]" 'vimpulse-jump-to-tag-at-point)
+(define-key viper-vi-basic-map "\C-t" 'pop-tag-mark)
 ;; Map undo and redo from XEmacs' redo.el
-(define-key viper-vi-global-user-map "u"    'undo)
-(define-key viper-vi-global-user-map "\C-r" 'redo)
+(define-key viper-vi-basic-map "u" 'undo)
+(define-key viper-vi-basic-map "\C-r" 'redo)
 
 ;; Window manipulation
-(define-key viper-vi-global-user-map "\C-w" (make-sparse-keymap))
-(define-key viper-vi-global-user-map "\C-w\C-w" 'vimpulse-cycle-windows)
-(define-key viper-vi-global-user-map "\C-ww" 'vimpulse-cycle-windows)
-(define-key viper-vi-global-user-map "\C-wo" 'delete-other-windows)
-(define-key viper-vi-global-user-map "\C-wc" 'delete-window)
-(define-key viper-vi-global-user-map "\C-ws" 'split-window-vertically)
-(define-key viper-vi-global-user-map "\C-wv" 'split-window-horizontally)
-(add-to-list 'ex-token-alist '("on"    (delete-other-windows)))
-(add-to-list 'ex-token-alist '("only"  (delete-other-windows)))
-(add-to-list 'ex-token-alist '("clo"   (delete-window)))
+(define-key viper-vi-basic-map "\C-w" (make-sparse-keymap))
+(define-key viper-vi-basic-map "\C-w\C-w" 'vimpulse-cycle-windows)
+(define-key viper-vi-basic-map "\C-ww" 'vimpulse-cycle-windows)
+(define-key viper-vi-basic-map "\C-wo" 'delete-other-windows)
+(define-key viper-vi-basic-map "\C-wc" 'delete-window)
+(define-key viper-vi-basic-map "\C-ws" 'split-window-vertically)
+(define-key viper-vi-basic-map "\C-wv" 'split-window-horizontally)
+(add-to-list 'ex-token-alist '("on" (delete-other-windows)))
+(add-to-list 'ex-token-alist '("only" (delete-other-windows)))
+(add-to-list 'ex-token-alist '("clo" (delete-window)))
 (add-to-list 'ex-token-alist '("close" (delete-window)))
 
 (when (fboundp 'windmove-left)
-  (define-key viper-vi-global-user-map "\C-wh" 'windmove-left)
-  (define-key viper-vi-global-user-map "\C-wj" 'windmove-down)
-  (define-key viper-vi-global-user-map "\C-wk" 'windmove-up)
-  (define-key viper-vi-global-user-map "\C-wl" 'windmove-right))
+  (define-key viper-vi-basic-map "\C-wh" 'windmove-left)
+  (define-key viper-vi-basic-map "\C-wj" 'windmove-down)
+  (define-key viper-vi-basic-map "\C-wk" 'windmove-up)
+  (define-key viper-vi-basic-map "\C-wl" 'windmove-right))
 
 ;;; Insert mode keys
 ;; Vim-like completion keys
-(define-key viper-insert-global-user-map "\C-p" 'dabbrev-expand)
-(define-key viper-insert-global-user-map "\C-n" 'vimpulse-abbrev-expand-after)
-;; (define-key viper-insert-global-user-map [backspace] 'backward-delete-char-untabify) ; vim doesn't do this!
-(define-key viper-insert-global-user-map [delete] 'delete-char) ;; delete key
+(define-key viper-insert-basic-map "\C-p" 'dabbrev-expand)
+(define-key viper-insert-basic-map "\C-n" 'vimpulse-abbrev-expand-after)
+;; (define-key viper-insert-basic-map [backspace] 'backward-delete-char-untabify) ; vim doesn't do this!
+(define-key viper-insert-basic-map [delete] 'delete-char) ;; delete key
                                         ; make ^[ work
-(define-key viper-insert-global-user-map (kbd "ESC") 'viper-exit-insert-state)
+(define-key viper-insert-basic-map (kbd "ESC") 'viper-exit-insert-state)
 
 ;;; My code (Alessandro)
 (defun vimpulse-indent-lines (count)
@@ -189,7 +195,7 @@ If REPLACE is non-nil, may overwrite bindings in MAP."
   (interactive)
   (select-window (next-window)))
 
-(defun vimpulse-search-for-symbol-at-point (forward-p &optional pos)
+(defun vimpulse-search-for-symbol-at-point (forward-p &optional pos force)
   "Search forwards or backwards for the symbol under point.
 FORWARD-P specifies the direction, POS the position from where
 to start the search."
@@ -205,10 +211,11 @@ to start the search."
     (cond
      ((stringp str)
       (setq str (regexp-quote str))
-      (setq str (concat "\\<" str "\\>" "\\|" "\\_<" str "\\_>"))
+      (setq str (concat "\\_<" str "\\_>"))
       ;; If searching several times in a row,
       ;; use the same search string each time
-      (when (or (string= "" viper-s-string)
+      (when (or force
+                (string= "" viper-s-string)
                 (not (looking-at-p viper-s-string)))
         (setq viper-s-string str))
       (setq viper-s-forward forward-p)
@@ -218,11 +225,11 @@ to start the search."
 
 (defun vimpulse-search-forward-for-symbol-at-point ()
   (interactive)
-  (vimpulse-search-for-symbol-at-point t))
+  (vimpulse-search-for-symbol-at-point t nil t))
 
 (defun vimpulse-search-backward-for-symbol-at-point ()
   (interactive)
-  (vimpulse-search-for-symbol-at-point nil))
+  (vimpulse-search-for-symbol-at-point nil nil t))
 
 (defun vimpulse-goto-definition ()
   (interactive)
@@ -232,6 +239,18 @@ to start the search."
   (interactive)
   (let ((tag (thing-at-point 'word)))
     (find-tag tag)))
+
+(defadvice viper-paren-match (around vimpulse activate)
+  "Go to percentage in the file when ARG >= 10."
+  (let ((val (viper-p-val arg)))
+    (cond
+     ((<= 10 val)
+      (goto-char (+ (point-min)
+                    (floor (* (- (point-max) (point-min)) 0.01
+                              (max 0 (min 100 val))))))
+      (beginning-of-line))
+     (t
+      ad-do-it))))
 
 ;;; cppjavaperl's code
 (defun vimpulse-abbrev-expand-after ()
