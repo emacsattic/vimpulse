4002969776b8817a157d111b23dfd4b5cf065508
epsilon47 <epsilon47@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-04-05 17:00:31 +0000

    Made :q and :q! work like in Vim, regardless of Viper level.

--- vimpulse-ex.el
+++ vimpulse-ex.el
@@ -5,29 +5,28 @@
   '(("b" "buffer")
     ("bdelete" (vimpulse-kill-current-buffer))
     ("bnext" "next")
-    ("syntax" (global-font-lock-mode))
+    ("clo" "close")
+    ("close" (delete-window))
+    ("on" "only")
+    ("only" (delete-other-windows))
+    ("quit" (save-buffers-kill-emacs))
     ("split" (split-window))
+    ("syntax" (global-font-lock-mode))
     ;; Emacs and Vim use inverted naming conventions for splits
-    ("vsplit" (split-window-horizontally))))
+    ("vsplit" (split-window-horizontally)))
+  "Extra Ex commands, added to `ex-token-alist' when Vimpulse loads.")
 
 (defun vimpulse-kill-current-buffer ()
   "Kill the current buffer."
   (interactive)
   (kill-buffer nil))
 
-;;; Manipulation of Vipers functions by using the advice feature
-
-;; Many of the functions here rely as heavily on Viper's internals
-;; as Viper itself
-(defadvice viper-ex (around vimpulse-extended-ex-commands
-                            (arg &optional string)
-                            activate)
-  ad-do-it)
-
 ;; Additional Ex mode features: `ex-token-alist' is defined as a
 ;; constant, but it appears I can safely push values to it!
-(setq ex-token-alist
-      (append vimpulse-extra-ex-commands ex-token-alist))
+(dolist (entry vimpulse-extra-ex-commands)
+  (setq ex-token-alist
+        (delete (assoc (car entry) ex-token-alist) ex-token-alist))
+  (add-to-list 'ex-token-alist entry t))
 
 (provide 'vimpulse-ex)
 
