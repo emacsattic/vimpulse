b89d25691a73b3cf55ff5e3a6782e9bce7f2bbdc
Vegard Øye <vegard_oye@hotmail.com>
2010-06-01 16:48:00 +0200

    Regression test for Visual replace

--- vimpulse-test.el
+++ vimpulse-test.el
@@ -1138,7 +1138,15 @@ This line is not included in the report."
      "This buffer is for notes you don't want to save, and for \
 Lisp evaluation.
 If you want to create a file, visit that file with C-x C-f,
-then enter the text in that file's own buffer.\n")))
+then enter the text in that file's own buffer.\n"))
+  (test-visual-replace
+   "Visually replace parts of two lines."
+   (execute-kbd-macro "wvjra")
+   (assert-string=
+     (buffer-string)
+     ";; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaf you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.\n")))
 
 (provide 'vimpulse-test)
 
