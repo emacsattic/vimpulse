44bf2e88112db6797ca2f6e977c6f75fb961a486
Vegard Øye <vegard_oye@hotmail.com>
2010-09-03 17:38:20 +0200

    Provide overview of keybinding functions
    The comment block of vimpulse-modal.el focuses excessively on careful
    bindings, so narrow it down and shed some light on the the other
    utilities.

--- vimpulse-modal.el
+++ vimpulse-modal.el
@@ -1,132 +1,36 @@
 ;;;; Modal keybinding functions
 
-;; This provides the functions `vimpulse-map', `vimpulse-imap',
-;; `vimpulse-vmap' and `vimpulse-omap', which mimic :map, :imap, :vmap
-;; and :omap in Vim, as well as `vimpulse-make-careful-binding', which
-;; makes bindings "on top of" previous bindings.
+;; Functions for making state bindings (modal bindings).
 ;;
-;; BACKGROUND
+;; Global state bindings (seen in all buffers):
+;;     `vimpulse-global-set-key', `vimpulse-map', `vimpulse-imap',
+;;     `vimpulse-vmap', `vimpulse-omap'
+;; Buffer-local state bindings (seen only in the current buffer):
+;;     `vimpulse-local-set-key', `vimpulse-map-local',
+;;     `vimpulse-imap-local', `vimpulse-vmap-local',
+;;     `vimpulse-omap-local'
+;; State bindings for Emacs modes (minor and major):
+;;     `vimpulse-define-key'
 ;;
-;; The :map, :imap, :vmap and :omap commands of Vim let one make two
-;; key mappings starting with the same sequence of characters without
-;; one overwriting the other. For example:
+;; `vimpulse-map' etc. mimic Vim's :map, :imap, :vmap and :omap
+;; commands; `vimpulse-map-local' etc. are similar, but buffer-local.
 ;;
-;;     :imap aa foo
-;;     :imap aaa bar
+;; `vimpulse-define-key' associates state bindings with an Emacs mode,
+;; and is useful for grouping bindings:
 ;;
-;; When Vim has read "aa" in Insert mode, it will wait for another
-;; character to decide whether to insert "foo" or "bar". If the user
-;; types "a", "bar" is inserted; if another letter, "foo" plus that
-;; letter.
+;;     (define-minor-mode foo-mode)
+;;     (vimpulse-define-key 'foo-mode 'vi-state "a" 'bar)
+;;     (vimpulse-define-key 'foo-mode 'visual-state "b" 'baz)
 ;;
-;; Compare with the analogous use of Emacs' `global-set-key' function:
+;; These bindings are only seen in buffers where foo-mode is enabled.
+;; You can also augment existing modes, of course.
 ;;
-;;     (global-set-key "aa" 'foo)
-;;     (global-set-key "aaa" 'bar)
-;;
-;; Here, the first binding is simply overwritten by the more specific
-;; second. The end result is that "aaa" is bound to `bar', while any
-;; other sequence starting with "aa" is not bound to anything.
-;;
-;; The solution is a set of Vim-like or "modal" functions for making
-;; new key bindings "on top of" previous bindings. They are
-;; `vimpulse-map', `vimpulse-imap', `vimpulse-vmap' and
-;; `vimpulse-omap', which mimic Vim's commands, and
-;; `vimpulse-make-careful-binding', a general function for specifying
-;; the keymap. Returning to the example:
-;;
-;;     (vimpulse-imap "aa" 'foo)
-;;     (vimpulse-imap "aaa" 'bar)
-;;
-;; This will bind "aaa" to `bar', and "aa" + any other key to `foo'.
-;; The syntax is the same as that of `global-set-key'. The key
-;; sequence may be specified as a string, like above, as a vector
-;; (like [?a ?b ?c]), or as a call to `kbd' (like (kbd "a b c")).
-;;
-;; To make a binding in vi (command) mode, use `vimpulse-map'; in
-;; Insert mode, `vimpulse-imap'; in Visual mode, `vimpulse-vmap'; in
-;; Operator-Pending mode, `vimpulse-omap'. The more general
-;; `vimpulse-make-careful-binding' function lets one specify the
-;; keymap to store the binding in, as when using `define-key':
-;;
-;;     (vimpulse-make-careful-binding keymap "abc" 'command)
-;;
-;; IMPLEMENTATION
-;;
-;; The code depends on a little-known GNU Emacs feature called
-;; "default key bindings". A default key binding is a binding ending
-;; with the Lisp symbol t, which roughly stands for "any other key".
-;; Default bindings allow a keymap to bind all possibilities without
-;; having to enumerate them. For example, we may bind the sequence
-;; "AB" + any key as such:
-;;
-;;     (global-set-key (kbd "A B <t>") 'foo)
-;;
-;; This means that "ABA" will execute `foo', as will "ABB", "ABC",
-;; and so on. For more on default key bindings, see the GNU Emacs
-;; Lisp Reference Manual, chapter 22.3: "Format of Keymaps".
-;;
-;; (info "(elisp)Format of Keymaps")
-;;
-;; What is done by functions like `vimpulse-make-careful-binding' and
-;; `vimpulse-map' (which depends on the former) is to generate these
-;; default bindings automatically. If "AB" is already bound to `foo'
-;; and we carefully bind "ABC" to `bar', the old binding is first
-;; replaced by a default binding, as if we issued the following:
-;;
-;;     (global-set-key (kbd "A B") nil) ; delete old binding
-;;     (global-set-key (kbd "A B <t>") 'foo)
-;;     (global-set-key (kbd "A B C") 'bar)
-;;
-;; Then, "ABC" runs `bar', while "AB" + any key other than C
-;; runs `foo'.
-;;
-;; This almost gets us where we want with regard to Vimpulse, but not
-;; quite. The problem is that quite a few commands must necessarily
-;; read and parse keyboard input to decide what to do. For instance,
-;; Viper binds "d" to the general command `viper-command-argument',
-;; which, depending on the next key-presses, deletes a line, two
-;; words, or any motion entered by the user. What happens if we decide
-;; to carefully bind, say, "dq" to a custom command `foo' of our own?
-;;
-;;     (global-set-key (kbd "d") nil) ; delete old binding
-;;     (global-set-key (kbd "d <t>") 'viper-command-argument)
-;;     (global-set-key (kbd "d q") 'foo)
-;;
-;; Now, if the user enters "dq", `foo' is called. But when the user
-;; enters "dw" to delete a word, `viper-command-argument' is called
-;; only after the "w" is entered. This destroys the logic of the
-;; command, which depends on "d" being the last key-press (stored in
-;; `last-command-event') before "w" is read through `read-char'. It
-;; obviously won't work as intended with a single "w" missing a
-;; preceding "d", which is what it sees.
-;;
-;; So, we need to find a way to pass "d" and "w" along in the proper
-;; manner; that is, to make the default binding appear the same as the
-;; old binding it replaces. This is done by
-;; `vimpulse-careful-pre-hook', which unreads "w" (so it can be read
-;; again) and changes `last-command-event' to "d". Of course, this
-;; behavior is only needed for default key bindings, and only for
-;; default key bindings made by careful bindings. To that end, every
-;; time `vimpulse-make-careful-binding' makes a default binding, the
-;; binding is listed in `vimpulse-careful-alist' for future reference.
-;; Checking against the list, `vimpulse-careful-pre-hook' only does
-;; its thing if the current binding comes back positive.
-;;
-;; XEmacs is somewhat fuzzy about its command loop variables, not
-;; allowing direct modification of `last-command-event'. However,
-;; shadowing it with a `let' binding is possible, and a wrap-around
-;; advice of the current command is employed to accomplish this. Also,
-;; XEmacs does not have default key bindings in quite the same way as
-;; GNU Emacs; `vimpulse-default-binding' takes care of the
-;; differences.
-;;
-;; LIMITATIONS
-;;
-;; Vim has a `timeout' option which lets one specify the time in
-;; milliseconds that is waited for a key code or mapped key sequence
-;; to complete. Emacs, on the other hand, will wait indefinitely. This
-;; behavior is probably not implementable.
+;; Vimpulse provides "careful" bindings, which are bindings that can
+;; be safely stacked on top of pre-existing bindings. They can be made
+;; with `vimpulse-make-careful-binding' (see its docstring for more
+;; details), or by passing a non-nil value for the CAREFUL argument of
+;; `vimpulse-global-set-key', `vimpulse-local-set-key' or
+;; `vimpulse-define-key'. `vimpulse-map' etc. are always careful.
 
 ;;; Advice
 
@@ -157,7 +61,7 @@ directly, but shadowing them with a `let' binding works."
 but only for bindings listed in `vimpulse-careful-alist'."
   (let ((temp-sequence (vimpulse-strip-prefix key-sequence)))
     (setq temp-sequence (vimpulse-truncate temp-sequence -1))
-    (and this-command ; may be nil
+    (and this-command                     ; may be nil
          (not (key-binding key-sequence)) ; only default bindings
          (eq (cdr (assoc temp-sequence vimpulse-careful-alist))
              this-command))))
@@ -279,15 +183,16 @@ overwrite the old. E.g., if we want to carefully bind \"A B C\" to
     \"A B C\"   => `foo'
     \"A B <t>\" => `bar'
 
-which means that \"A B D\", for example, defaults to `bar'. (For
-more on default bindings, see `define-key'.) The default binding
-gets listed in `vimpulse-careful-alist', so that, with regard to
-command loop variables, it appears exactly the same as the
-binding it replaced. To override this, use DONT-LIST.
-DEFINE-FUNC specifies a function to be used in place of
-`define-key'.
+Thus, \"A B C\" runs `foo', while \"A B\" + any key other than \"C\"
+defaults to `bar'. To remove a careful binding, bind it to nil.
 
-To remove a binding, bind it to nil.
+The default binding gets listed in `vimpulse-careful-alist', so
+that, with regard to command loop variables, it appears exactly
+the same as the binding it replaced. In the example above, `bar'
+will see a `last-command-event' of \"B\", and will automatically
+obtain the last key by calling `read-char'. To make a regular
+default binding, use DONT-LIST. DEFINE-FUNC specifies a function
+to be used in place of `define-key'.
 
 NOTE: If the original binding \"A B\" is not stored in KEYMAP,
 but in some other map which is active only in a certain
@@ -326,7 +231,7 @@ only if called in the same state. The functions `vimpulse-map',
                 (or (key-binding temp-sequence t) previous-binding)))
         (setq previous-binding current-binding)
         ;; If `current-binding' is a keymap, do nothing, since our
-        ;; careful binding can exist happily as part of that keymap.
+        ;; careful binding can happily exist as part of that keymap.
         ;; However, if `current-binding' is a command, we need to make
         ;; room for the careful binding by creating a default binding.
         (unless (keymapp current-binding)
